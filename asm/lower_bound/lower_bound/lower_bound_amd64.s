84e2accd6d3471962b7aaa34163b57d7a6a6bdba
TimurUrazov <tu177086@gmail.com>
2025-01-28 15:22:42 +0300

    add amd pt.2

--- asm/lower_bound/lower_bound/lower_bound_amd64.s
+++
@@ -1,4 +0,0 @@
-#include "textflag.h"
-
-// func LowerBound(slice []int64, value int64) int64
-TEXT ·LowerBound(SB), NOSPLIT, $0
--- asm/word_count/word_count/word_count_amd64.s
+++
@@ -1,4 +0,0 @@
-#include "textflag.h"
-
-// func WordCount(data []rune) int32
-TEXT ·WordCount(SB), NOSPLIT, $0
